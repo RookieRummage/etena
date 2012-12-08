8e06c8b330d1bb591633452085fde1e66429eb39
Kavitha <kavithar@thoughtworks.com>
2012-12-08 11:11:59 +0530

    Kavitha|Sasi - First commit; created the project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etena::Application.config.secret_token = 'bccc8fea918ade370b1809bee296f4d140271ad4b6f9af5a17ef4ac4dc683133747c5b82e9d12c8d2eaea74303ad9791de1de6ccdc2aa3e51ea971414f399f3c'
